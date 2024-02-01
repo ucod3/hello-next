61bccddd94054055102a96dda6b3d55e96605f51
Usman Butt <usman.butt@outlook.com>
2024-02-01 12:35:53 -0500

    Add rendered time to footer in RootLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,12 @@ import React from 'react';
 import './styles.css';
 
 function RootLayout({ children }) {
+  const renderedTime = new Date().toLocaleString();
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
         {children}
-
+        <footer>Page rendered on {renderedTime}</footer>
       </body>
     </html>
   );
